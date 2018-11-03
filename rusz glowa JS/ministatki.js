4dba6f0b5153d8718b5e03a549402e3da8732371
Karambix\Karambii <pichow@wp.pl>
2018-11-03 14:53:31 +0100

    ship battle with browser alerts

---
+++ rusz glowa JS/ministatki.js
@@ -0,0 +1,33 @@
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 2;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+var match = "\/d";
+var hitText = "Trafiony!!";
+
+while (isSunk == false) {
+    guess = prompt("Gotów, cel, pal! (podaj liczbę z zakresu od 0-6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Proszę podać prawidłowy numer.");
+    } else {
+        guesses = guesses +1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits +1; 
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("Trafiony zatopiony!");
+            }
+        }
+    }
+}
+
+var stats = "Potrzebowałeś "+guesses+" prób, by zatopić okręt, " 
++ "czyli Twoja efektywność wynosi: " + (3/guesses)*100 + "%.";
+alert(stats);
